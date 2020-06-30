2651e18aa7bf626035351ce6d7c0e1e62ce92b85
Sara <saracorsob@gmail.com>
2020-06-30 17:14:58 +0200

    Semi-correzione

--- js/script.js
+++ js/script.js
@@ -6,78 +6,106 @@
 
 $(document).ready(function() {
   // Variabile che stabilisce la data inziale
-  var currentMonth = $('.days').attr('data-month');
-  var currentMonthNumber = moment(currentMonth).month();
+  var currentDate = '2018-01-01';
+  var currentDateNumber = moment(currentDate).month();
 
   // Variabile che stabilisce la data corrente
-  var currentMonthMoment = moment(currentMonth).format('MMMM');
+  var currentDateMoment = moment(currentDate).format('MMMM');
+
+  printMonth(currentDate, currentDateMoment);
+  printFestivity(currentDateNumber);
 
   $('.precedente').click(function() {
-    if(currentMonthNumber > 0 || currentMonthNumber <= 11) {
-      var preMonth = moment(currentMonth).subtract(1, 'months');
-      $('.days').attr("data-month", preMonth.format('YYYY-MM-DD'));
+    if(currentDateNumber > 0 || currentDateNumber <= 11) {
+      var currentMonth = $('.days').attr('data-month');
+      var currentMonthMoment = moment(currentMonth).subtract(1, 'months');
+      // $('.days').attr("data-month", prevMonth.format('YYYY-MM-DD'));
+      var prevMonth = currentMonthMoment.format('YYYY-MM-DD');
+
+      printMonth(prevMonth, currentMonthMoment.format('MMMM'));
+      printFestivity(currentMonthMoment.month());
     }
   });
 
   $('.successivo').click(function() {
-    if(currentMonthNumber >= 0 || currentMonthNumber < 11) {
-      var succMonth = moment(currentMonth).add(1, 'months');
-      $('.days').attr("data-month", succMonth.format('YYYY-MM-DD'));
+    if(currentDateNumber >= 0 || currentDateNumber < 11) {
+      var currentMonth = $('.days').attr('data-month');
+      var currentMonthMoment = moment(currentMonth).add(1, 'months');
+      // $('.days').attr("data-month", succMonth.format('YYYY-MM-DD'));
+      var succMonth = currentMonthMoment.format('YYYY-MM-DD');
+
+      printMonth(succMonth, currentMonthMoment.format('MMMM'));
+      printFestivity(currentMonthMoment.month());
     }
   });
 
-  // HANDLEBARS per completare il mese
-  var templateMonth = $('#month-template').html();
-  var templateMonthToCompile = Handlebars.compile(templateMonth);
 
-  var contextMonth = {month: currentMonthMoment};
-  var templateMonthCompiled = templateMonthToCompile(contextMonth);
-  $('.calendario').prepend(templateMonthCompiled);
 
-  // Variabile che definisce la lunghezza del mese corrente
-  var lengthMonth = moment(currentMonth).daysInMonth();
+  // FUNCTIONS
 
-  // HANDLBARS per completare la lista
-  var templateDays = $('#days-template').html();
-  var templateDaysToCompile = Handlebars.compile(templateDays);
+  function printMonth(currentDate, currentDateMoment) {
+    $('h2').html('');
+    $('.days').html('');
 
-  // Ciclo for per riempire e incollare il template copiato
-  // Con giorno e mese
-  for (var i = 0; i < lengthMonth; i++) {
-    var contextDays = {
-      day: (i + 1) + ' ' + currentMonthMoment,
-    };
-    var templateDaysCompiled = templateDaysToCompile(contextDays);
+    // HANDLEBARS per completare il mese
+    var templateMonth = $('#month-template').html();
+    var templateMonthToCompile = Handlebars.compile(templateMonth);
 
-    $('.calendario .days').append(templateDaysCompiled);
+    var contextMonth = {month: currentDateMoment};
+    var templateMonthCompiled = templateMonthToCompile(contextMonth);
+    $('.calendario').prepend(templateMonthCompiled);
+
+    var currentDateFormat = moment(currentDate).format('YYYY-MM-DD');
+    $('.days').attr('data-month', currentDateFormat)
+
+    // Variabile che definisce la lunghezza del mese corrente
+    var lengthMonth = moment(currentDate).daysInMonth();
+
+    // HANDLBARS per completare la lista
+    var templateDays = $('#days-template').html();
+    var templateDaysToCompile = Handlebars.compile(templateDays);
+
+    // Ciclo for per riempire e incollare il template copiato
+    // Con giorno e mese
+    for (var i = 1; i <= lengthMonth; i++) {
+      var contextDays = {
+        day: i + ' ' + currentDateMoment,
+      };
+      var templateDaysCompiled = templateDaysToCompile(contextDays);
+
+      $('.calendario .days').append(templateDaysCompiled);
+    }
   }
 
-  // Chiamata Ajax per scrivere la festività
-  $.ajax(
-    {
-      url: "https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0",
-      method: "GET",
-      // Aggiungi variabile per stabilire il mese
-      data: {month: "0" },
-      success: function(data) {
 
-        if (data.response.length !== 0) {
-          for (var i = 0; i < data.response.length; i++) {
-            var date = moment(data.response[i].date).format('d MMMM');
-            $('.days li').each(function() {
+  function printFestivity(currentMonthNumber) {
+    // Chiamata Ajax per scrivere la festività
+    $.ajax(
+      {
+        url: "https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0",
+        method: "GET",
+        // Aggiungi variabile per stabilire il mese
+        data: {month: currentMonthNumber },
+        success: function(data) {
 
-              if($(this).text() === date) {
-                $(this).append(' - ' + data.response[i].name);
-                $(this).addClass('festivity');
-              }
-            });
+          if (data.response.length !== 0) {
+            for (var i = 0; i < data.response.length; i++) {
+              var date = moment(data.response[i].date).format('d MMMM');
+              $('.days li').each(function() {
+
+                if($(this).text() === date) {
+                  $(this).append(' - ' + data.response[i].name);
+                  $(this).addClass('festivity');
+                }
+              });
+            }
           }
+        },
+        error: function() {
+          alert('Si è verificato un errore!');
         }
-      },
-      error: function() {
-        alert('Si è verificato un errore!');
       }
-    }
-  );
+    );
+  }
 
 });
